fce95e1e677e5893aca64df418f7a20988256b12
hyeok9855 <hyeok9855@gmail.com>
2025-08-17 08:56:37 -0400

    include EUBO in verbose printing

--- utils/print_util.py
+++ utils/print_util.py
@@ -1,11 +1,12 @@
 def print_results(step, logger, config):
     if config.verbose:
-        try:
-            print(
-                f'Step {int(step)}: ELBO {float(logger["KL/elbo"][-1])}; Δ lnZ {float(logger["logZ/reverse"][-1])}; '
-                f'reverse_ESS {float(logger["ESS/reverse"][-1])}'
-            )
-        except:
-            print(
-                f'Step {int(step)}: ELBO {float(logger["KL/elbo"][-1])}; Δ lnZ {float(logger["logZ/reverse"][-1])}'
-            )
+        string = f"Step {int(step)}: ELBO {float(logger['KL/elbo'][-1]):.4f}; "
+        string += f"IW-ELBO {float(logger['logZ/reverse'][-1]):.4f}; "
+        if "KL/eubo" in logger:
+            string += f"EUBO {float(logger['KL/eubo'][-1]):.4f}; "
+
+        string += f"reverse_ESS {float(logger['ESS/reverse'][-1]):.6f}; "
+        if "ESS/forward" in logger:
+            string += f"forward_ESS {float(logger['ESS/forward'][-1]):.6f}"
+
+        print(string)
